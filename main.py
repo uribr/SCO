338dacda3cec2c6ff8363cfe17d7920472b79dc1
Uri Bracha <uribracha@mail.tau.ac.il>
2024-09-11 22:12:13 +0300

    Added regularization. I haven't tested it yet.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from gd_utils import *
 VALIDATION_SET_RELATIVE_SIZE = 0.2
 TRAINING_SET_RELATIVE_SIZE = 0.6
 TEST_SET_RELATIVE_SIZE = 0.2
+REGULARIZATION_COEFFICIENT = 0.2
 # Selecting 2 classes (digits) to make it binary
 SELECTED_CLASSES = [0, 9]
 LEARNING_RATE = 0.01
@@ -19,8 +20,14 @@ BATCH_SIZE = 64
 NUM_EPOCHS = 5
 
 
+def gradient_descent():
+    pass
 
-def main(learning_rate, batch_size, number_of_epochs, selected_classes, verbose):
+
+
+
+
+def main(learning_rate, batch_size, number_of_epochs, selected_classes, regularization_coefficient, stochastic, projected, verbose):
     if verbose:
         print('Starting...')
     # Load and preprocess data
@@ -76,7 +83,19 @@ def main(learning_rate, batch_size, number_of_epochs, selected_classes, verbose)
         epoch_loss = bce_loss(logits, train_targets)
         grads = bce_grad(logits, train_targets, train_data)
 
-        weights = update_weights_vanilla(weights, grads, learning_rate)
+        new_weights = update_weights_vanilla(weights, grads, learning_rate)
+        if regularization_coefficient is not None:
+            # TODO (Uri) - Should the weights in the regularization term be the new weights from the vanilla update rule or the old weights?
+            weights = new_weights + 2 * regularization_coefficient * np.linalg.norm(weights)
+        elif projected:
+            # TODO - Implement
+            raise NotImplementedError()
+        elif stochastic:
+            # TODO - Implement
+            raise NotImplementedError()
+        else:
+            weights = new_weights
+
     if verbose:
         print('Terminating...')
 
@@ -84,10 +103,16 @@ if __name__ == '__main__':
     # TODO (Uri) - Added some arguments. Will probably need to update this at some point.
     parser = argparse.ArgumentParser()
     parser.add_argument('--labels', help='The labels for binary classification (e.g., "--labels 0 9" means [0, 9])', type=int, nargs=2, default=SELECTED_CLASSES)
+    parser.add_argument('-v', '--verbose', help='Prints extra information and details', action='store_true')
+    parser.add_argument('-r', '--regularized', help='Use regularized gradient descent', type=float, default=None)
+    parser.add_argument('-s', '--stochastic', help='Use stochastic gradient descent', action='store_true')
+    parser.add_argument('-p', '--projected', help='Use projected gradient descent', action='store_true')
+
     parser.add_argument('--epochs', help='Number of epochs', type=int, default=NUM_EPOCHS)
     parser.add_argument('--rate', help='Learning rate', type=float, default=LEARNING_RATE)
     parser.add_argument('--batch', help='Batch size', type=int, default=BATCH_SIZE)
-    parser.add_argument('-v', '--verbose', help='Prints extra information and details', action='store_true')
     args = parser.parse_args()
 
-    main(args.rate, args.batch, args.epochs ,args.labels, args.verbose)
+    main(args.rate, args.batch, args.epochs ,args.labels,
+         args.regularized, args.stochastic, args.projected,
+         args.verbose)
