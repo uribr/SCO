786c6141769d0683410cf7d9071164613f544536
Uri Bracha <uribracha@mail.tau.ac.il>
2024-09-29 18:37:43 +0300

    Small printing bug

--- plot_utils.py
+++ plot_utils.py
@@ -2,13 +2,14 @@
 def build_plot_text(learning_rate, selected_classes, number_of_epochs,
                     loss_function_name, regularization_coefficient,
                     hypersphere_radius, stochastic, train_accuracy,
-                    validation_accuracy):
+                    validation_accuracy, test_accuracy=.0):
     plot_text = f'Digits: {selected_classes[0]}, {selected_classes[-1]}\n'\
                 f'Rate: {learning_rate}\n'\
                 f'Iterations: {number_of_epochs}\n'\
                 f'Loss: {loss_function_name}\n'\
                 f'Train Acc: {train_accuracy:.2f}\n'\
-                f'Validation Acc: {validation_accuracy:.2f}\n'\
+                f'Valid Acc: {validation_accuracy:.2f}\n' \
+                f'Test Acc: {test_accuracy:.2f}\n' \
                 'Variant: '
     if regularization_coefficient is not None:
         plot_text += 'RGD\n'
